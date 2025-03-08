d3d3d91952b73d1851c1a3a2bb36515b5eca689e
Colin Paul <colinpaul.ebby2022@vitstudent.ac.in>
2025-03-08 15:19:57 +0530

    Add comment to trigger GitHub Actions workflow

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from applicaton import create_app
 
 app = create_app()
 
-# Test comment to trigger GitHub Actions
+# Test comment to trigger GitHub Actions workflow
 if __name__ == "__main__":
     app.run(debug=True)
